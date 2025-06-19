eec64667656667726695a97e24a84c8b2639f1de
Teddy Schmitz <tedsch@stripe.com>
2025-06-19 11:20:15 +0100

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fx_app/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
